b216b4f2b6e15743676b6f44eac476c7371d75f6
mrkunal0430@gmail.com <mrkunal0430@gmail.com>
2026-02-01 18:41:09 +0530

    initialize Express server with database connection, CORS, and core API routes.

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://capton-visa-point.vercel.app",
-      "https://www.captonvisapoint.com/"
+      "https://www.captonvisapoint.com"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
